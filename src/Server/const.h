e0777a1b1cc0d066a1da85524c29b9734427f67b
dew-x <andreu.figueras@gmail.com>
2016-05-11 16:18:05 +0200

    added func + pi

--- src/Server/const.h
+++ src/Server/const.h
@@ -1,3 +1,9 @@
 #pragma once
 const int port = 50000;
 const unsigned BUFSIZE = 512;
+const float M_PI = 3.1415927410125732421875;
+
+inline float RadToDeg(float Rad)
+{
+	return Rad / M_PI * 180.f;
+}
