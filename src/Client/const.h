2025b6011ae0b994dfecd0e18994827de0bd188f
dew-x <andreu.figueras@gmail.com>
2016-05-11 17:40:01 +0200

    fix map size

--- src/Client/const.h
+++ src/Client/const.h
@@ -4,9 +4,9 @@ const unsigned BUFSIZE = 512;
 const unsigned NICKSIZE = 12;
 const unsigned MINNICK = 3;
 const char SERVERIP[10] = "localhost";
-const unsigned MWIDTH = 1000;
-const unsigned MHEIGHT = 1000;
-const unsigned MSIZE = 100;
+const unsigned MWIDTH = 100;
+const unsigned MHEIGHT = 100;
+const unsigned MSIZE = 200;
 const float SPEED = 0.3f;
 
 const float M_PI = 3.1415927410125732421875;
