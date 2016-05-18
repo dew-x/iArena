2ff88de484a046dc4a90428f9ab0e473d69bd972
mastergun <mferragut@enti.cat>
2016-05-18 19:01:13 +0200

    retoque

--- src/Client/functions.h
+++ src/Client/functions.h
@@ -8,7 +8,7 @@ inline sf::Vector2f dot(sf::Vector2f a, sf::Vector2f b) {
 }
 
 inline sf::Vector2f normalize(sf::Vector2f a) {
-	float module = sqrt((a.x*a.x) + (a.y*a.y));
+	float module = magnitude(a);
 	if (module == 0) return{ 0.0f, 0.0f};
 	else return{a.x/module, a.y/module};
 }
